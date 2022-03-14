10c7d2a9f1f37d3b195260e854e267578c6fb267
Jonathan Willian <jonathanwc.pinheiro@outlook.com>
2022-03-14 15:54:12 -0300

    adding more sql scripts

---
+++ sql/PopulatePixKeyTable.sql
@@ -0,0 +1,32 @@
+USE Pix;
+
+
+INSERT INTO PIX_KEY 
+(ID, KEY_TYPE, KEY_VALUE, AGENCY_NUMBER, ACCOUNT_TYPE, ACCOUNT_NUMBER, ACCOUNT_HOLDER_NAME, ACCOUNT_HOLDER_LAST_NAME, PERSON_TYPE, DATETIME_INCLUSION)
+VALUES 
+(NEWID(), 'email', 'maria@email.com', 1546, 'corrente', 1468543, 'Maria', 'Araujo', 'fisica', GETDATE()),
+(NEWID(), 'celular', '+55011985497', 1546, 'corrente', 1468543, 'Maria', 'Araujo', 'fisica', GETDATE()),
+(NEWID(), 'cpf', '45453716900', 1546, 'corrente', 1468543, 'Maria', 'Araujo', 'fisica', GETDATE()),
+(NEWID(), 'celular', '+55011985497211', 1546, 'corrente', 1468543, 'Maria', 'Araujo', 'fisica', GETDATE()),
+(NEWID(), 'celular', '+55011985497315', 1546, 'corrente', 1468543, 'Maria', 'Araujo', 'fisica', GETDATE()),
+(NEWID(), 'cnpj', '015743514110', 1834, 'corrente', 1548513, 'Jose', 'Santos', 'Juridica', GETDATE()),
+(NEWID(), 'cpf', '1505215441', 1834, 'corrente', 1548513, 'Jose', 'Santos', 'Juridica', GETDATE()),
+(NEWID(), 'celular', '+55015203510016', 1834, 'corrente', 1548513, 'Jose', 'Santos', 'Juridica', GETDATE()),
+(NEWID(), 'celular', '+55010457411421', 1834, 'corrente', 1548513, 'Jose', 'Santos', 'Juridica', GETDATE()),
+(NEWID(), 'aleatorio', '013125684085', 1834, 'corrente', 1548513, 'Jose', 'Santos', 'Juridica', GETDATE()),
+(NEWID(), 'aleatorio', '011985497212', 1834, 'corrente', 1548513, 'Jose', 'Santos', 'Juridica', GETDATE()),
+(NEWID(), 'aleatorio', '011985497118', 1834, 'corrente', 1548513, 'Jose', 'Santos', 'Juridica', GETDATE()),
+(NEWID(), 'aleatorio', '5011985675212', 1834, 'corrente', 1548513, 'Jose', 'Santos', 'Juridica', GETDATE()),
+(NEWID(), 'aleatorio', '11973425612', 1834, 'corrente', 1548513, 'Jose', 'Santos', 'Juridica', GETDATE()),
+(NEWID(), 'aleatorio', '11956257889', 1834, 'corrente', 1548513, 'Jose', 'Santos', 'Juridica', GETDATE()),
+(NEWID(), 'aleatorio', '534011575878572', 1834, 'corrente', 1548513, 'Jose', 'Santos', 'Juridica', GETDATE()),
+(NEWID(), 'aleatorio', '87011787967385', 1834, 'corrente', 1548513, 'Jose', 'Santos', 'Juridica', GETDATE()),
+(NEWID(), 'aleatorio', '98795011634547568', 1834, 'corrente', 1548513, 'Jose', 'Santos', 'Juridica', GETDATE()),
+(NEWID(), 'aleatorio', '7896475011549696763', 1834, 'corrente', 1548513, 'Jose', 'Santos', 'Juridica', GETDATE()),
+(NEWID(), 'aleatorio', '7686018975843678', 1834, 'corrente', 1548513, 'Jose', 'Santos', 'Juridica', GETDATE()),
+(NEWID(), 'celular', '+55016878567587', 1834, 'corrente', 1548513, 'Jose', 'Santos', 'Juridica', GETDATE()),
+(NEWID(), 'aleatorio', '6757711658876786', 1834, 'corrente', 1548513, 'Jose', 'Santos', 'Juridica', GETDATE()),
+(NEWID(), 'celular', '+55018764776867', 1834, 'corrente', 1548513, 'Jose', 'Santos', 'Juridica', GETDATE()),
+(NEWID(), 'aleatorio', '87686016786786358', 1834, 'corrente', 1548513, 'Jose', 'Santos', 'Juridica', GETDATE()),
+(NEWID(), 'aleatorio', '86785014787867845', 1834, 'corrente', 1548513, 'Jose', 'Santos', 'Juridica', GETDATE()),
+(NEWID(), 'celular', '+55084587568567', 1834, 'corrente', 1548513, 'Jose', 'Santos', 'Juridica', GETDATE())
